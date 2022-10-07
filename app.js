c00ad57168aeb4ebe55f50c7484aa17ef6c8edc9
Naeemgg <naeemgg@duck.com>
2022-10-07 19:57:08 +0530

    done backend part

--- app.js
+++ app.js
@@ -1,46 +1,23 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"));
+let items = [];
 app.get("/", function (req, res) {
-  let day = new Date();
-  let today = day.getDay();
-  let date = day.getDate();
-  switch (today) {
-    case 0:
-      today = "Sunday";
-      break;
-
-    case 1:
-      today = "Monday";
-      break;
-
-    case 2:
-      today = "Tuesday";
-      break;
-
-    case 3:
-      today = "Wednesday";
-      break;
-
-    case 4:
-      today = "Thursday";
-      break;
-
-    case 5:
-      today = "Friday";
-      break;
-
-    case 6:
-      today = "Saturday";
-      break;
-
-    default:
-      console.log(`Error ${today}`);
-      break;
-  }
-  res.render("test", { currentDay: today,date:date });
+  const options = {weekday:"long",month:"long",day:"numeric",year:"numeric"};
+  let today = new Date();
+  today = today.toLocaleDateString("en-US",options);
+  res.render("test", { currentDay: today, items:items});
 });
+
+app.post("/",function (req,res) {
+  let item = req.body.todo;
+  items.push(item);
+  console.log(item);
+  res.redirect("/");
+});
+
 
 app.listen(3000, function () {
   console.log("http://localhost:3000");

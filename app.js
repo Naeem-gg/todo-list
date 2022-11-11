2c33ab28b19c9c115682d1b85cd701f4e864efc6
naeem <naeemqg1@gmail.com>
2022-11-11 23:07:56 +0530

    read data from mongodb

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}))
 app.set("view engine", "ejs");
-let items = ["learn React","learn Hooks", "learn props"];
+mongoose.connect("mongodb://localhost:27017/todoList",{useNewUrlParser: true});
+const itemSchema = new mongoose.Schema({
+  title: {type: String, required: true}
+});
+
+const Item = mongoose.model("Item",itemSchema);
+
+
+// let items = ["learn React","learn Hooks", "learn props"];
 app.get("/", function (req, res) {
-  const options = {weekday:"long",month:"long",day:"numeric",year:"numeric"};
-  let today = new Date();
-  today = today.toLocaleDateString("en-US",options);
-  res.render("test", { currentDay: today, items:items});
+  Item.find({},(err,itemsArray)=>{
+    if(err){
+      console.log(err);
+    }else{
+      // console.log(result); 
+      const options = {weekday:"long",month:"long",day:"numeric",year:"numeric"};
+      let today = new Date();
+      today = today.toLocaleDateString("en-US",options);
+      
+      if(itemsArray.length === 0){
+            const first = new Item({title: "The first item"});
+            const second = new Item({title: "The second item"});
+            const third = new Item({title: "The third item"});
+            const fourth = new Item({title: "The fourth item"});
+            
+            const defaultItem = [first, second, third, fourth];
+            
+            Item.insertMany(defaultItem, (err)=>{
+              if(err){
+                console.log(err);
+                }else{
+                console.log("Items inserted");
+                }
+            });
+            res.redirect("/");
+          }
+          else
+          {
+            
+            res.render("test", { currentDay: today, items:itemsArray });
+          }
+
+        }});
 });
 
 app.post("/",function (req,res) {

d84767c87304080af09132ba57aa9a74f0d52dba
naeem <naeemqg1@gmail.com>
2022-11-29 17:46:33 +0530

    update Atlas URI

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 
 //Atlas Mongodb
 
-mongoose.connect("mongodb+srv://naeem:Navjivan@testcluster.zhdjquu.mongodb.net/todoList");
+mongoose.connect("mongodb+srv://naeem:"+json.PASSWORD+"@testcluster.zhdjquu.mongodb.net/todoList");
 const itemSchema = new mongoose.Schema({
   name: String
 });
